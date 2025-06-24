55367d5126069014fa8102fa3e7e1a11491aa417
Micael Vitor <micael_vitor@yahoo.com.br>
2025-06-24 00:09:25 -0300

    chore: docker compose for developers

---
+++ public/xdebug.test.php
@@ -0,0 +1,15 @@
+<?php
+// phpinfo();
+// public/xdebug-test.php
+
+echo "Iniciando teste XDebug...";
+
+$a = 10;
+$b = 20;
+$sum = $a + $b;
+
+echo "A soma é: " . $sum;
+
+echo "Teste concluído!";
+
+exit;
