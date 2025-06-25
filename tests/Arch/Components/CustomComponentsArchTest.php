b1d13f0b5d56a39d84a6ba3ddd0823497b7a04e4
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-06-24 21:39:16 -0300

    test(custom-components): component should be registered at config/cms

--- tests/Arch/Components/CustomComponentsArchTest.php
+++ tests/Arch/Components/CustomComponentsArchTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Webid\Druid\Components\ComponentInterface;
 
 test('should implements ComponentInterface')
@@ -10,3 +11,21 @@ test('should implements ComponentInterface')
 it('should implement the interface methods')
     ->expect('app\Filament\Components')
     ->toHaveMethods(['blockSchema', 'fieldName', 'toBlade', 'toSearchableContent', 'imagePreview']);
+
+test('component should be registered at config/cms', function () {
+    $componentsConfig = Config::get('cms.components');
+    expect($componentsConfig)
+        ->toBeArray()
+        ->and($componentsConfig)->not->toBeEmpty('The "cms.components" configuration array is empty.');
+
+    foreach ($componentsConfig as $component) {
+        expect($component)->toHaveKey('class', $component['class']);
+
+        $componentClass = $component['class'];
+
+        expect(class_exists($componentClass))
+            ->and($componentClass)
+            ->toStartWith('App\Filament\Components' . '\\')
+            ->toHaveSuffix('Component');
+    }
+});
