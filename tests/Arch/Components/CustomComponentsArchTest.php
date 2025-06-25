a8069778c90d89e22150aef18b85848e06b0b032
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-06-24 21:43:13 -0300

    test(custom-components): component should be registered at TemplateRendererClass

--- tests/Arch/Components/CustomComponentsArchTest.php
+++ tests/Arch/Components/CustomComponentsArchTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\View\TemplateRenderer;
 use Illuminate\Support\Facades\Config;
+use Illuminate\View\ViewException;
 use Webid\Druid\Components\ComponentInterface;
 
 test('should implements ComponentInterface')
@@ -29,3 +31,8 @@ test('component should be registered at config/cms', function () {
             ->toHaveSuffix('Component');
     }
 });
+
+test('component should be registered at TemplateRendererClass', function () {
+    $templateRenderer = app(TemplateRenderer::class);
+    $templateRenderer->render('Component That is not Registered');
+})->throws(ViewException::class);
