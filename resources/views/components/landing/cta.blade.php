8ebe4d5299fbeed4bd570bb8594e17196392b1ec
moovmooov <mateusmoov@gmail.com>
2025-07-17 19:33:35 -0300

    feat: add BraSil flag image and update footer

--- resources/views/components/landing/cta.blade.php
+++ resources/views/components/landing/cta.blade.php
@@ -1,26 +1,27 @@
-<section class="text-base-content px-10 py-30 overflow-hidden bg-[#272727] ">
-    <div class="container bg-[#303030] rounded-3xl p-6 sm:p-10  mx-auto flex flex-col lg:flex-row lg:items-center lg:justify-between">
-            <div class="flex items-center justify-between gap-x-48 px-32 py-28">
-                <div class="shrink-0 mb-8 lg:mb-0">
-                    <span class="relative inline-block">
-                        <img src="{{ asset('images/blog_element.svg') }}" alt="Blog Icon"
-                            class="w-20 h-20 text-primary relative z-10" />
-                    </span>
-                </div>
-                <div class="flex-1 text-center lg:text-left mb-6 lg:mb-0 px-2 sm:px-0">
-                    <h3 class="text-2xl sm:text-5xl text-text-light font-extrabold mb-4">
-                        Acesse nosso blog para ter acesso as melhores métricas
-                    </h3>
-                    <p class="text-text-medium text-xl leading-[150%]">
-                        Nosso blog é o lugar ideal para quem busca conhecimento e insights para tomar decisões
-                        financeiras
-                        mais inteligentes.
-                    </p>
-                </div>
-                <div class="">
-                    <x-layout.shared.button class="p-4" href="{{ route('blog.index') }}" variant="primary">Acessar
-                        blog</x-layout.shared.button>
-                </div>
+<section class="text-base-content px-10 py-30 overflow-hidden bg-[#272727]">
+    <div
+        class="container bg-[#303030] rounded-3xl p-6 sm:p-10  mx-auto flex flex-col lg:flex-row lg:items-center lg:justify-between">
+        <div class="flex items-center justify-between gap-x-48 px-32 py-28">
+            <div class="shrink-0 mb-8 lg:mb-0">
+                <span class="relative inline-block">
+                    <img src="{{ asset('images/blog_element.svg') }}" alt="Blog Icon"
+                        class="w-20 h-20 text-primary relative z-10" />
+                </span>
+            </div>
+            <div class="flex-1 text-center lg:text-left mb-6 lg:mb-0 px-2 sm:px-0">
+                <h3 class="text-2xl sm:text-5xl text-text-light font-extrabold mb-4">
+                    Acesse nosso blog para ter acesso as melhores métricas
+                </h3>
+                <p class="text-text-medium text-xl leading-[150%]">
+                    Nosso blog é o lugar ideal para quem busca conhecimento e insights para tomar decisões
+                    financeiras
+                    mais inteligentes.
+                </p>
+            </div>
+            <div class="">
+                <x-layout.shared.button class="p-4" href="{{ route('blog.index') }}" variant="primary">Acessar
+                    blog</x-layout.shared.button>
             </div>
         </div>
+    </div>
 </section>
--- resources/views/components/layout/shared/footer.blade.php
+++ resources/views/components/layout/shared/footer.blade.php
@@ -1,41 +1,26 @@
 <footer class="relative bg-base-300 text-base-content py-16 ">
-    <div class="container mx-auto px-6 relative z-10">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 animate-fade-in">
-            <div class="space-y-6">
-                <a href="/" class="flex flex-col  gap-1 ">
+    <div class="mx-auto px-16 relative z-10">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-12 animate-fade-in items-start">
+
+            <div class="space-y-11 col-span-2">
+                <a href="/" class="flex flex-col gap-y-4">
                     <div class="flex items-center space-x-2">
-                        <x-logo :minimal="true" class="w-8 h-8"/>
-                        <p class="text-2xl font-semibold text-base-content">Fire|Ce</p>
+                        <x-logo-white class="w-[276px]" />
                     </div>
-                    <span class="text-base-content/70 text-sm">
+                    <span class="text-text-medium text-xl">
                         Investimentos Inteligentes e Planejamento Financeiro.
                     </span>
                 </a>
-
-                <!-- Social Icons -->
-                <div class="flex space-x-4 text-neutral">
-                    <!-- LinkedIn -->
-                    <a href="#"
-                       class="w-10 h-10 bg-base-200 hover:text-primary text-base-content/60 rounded-full flex items-center justify-center transition group shadow-lg overflow-hidden">
-                        <x-filament::icon icon="fab-linkedin" class="w-5 h-5 "/>
-                    </a>
-                    <!-- Instagram -->
-                    <a href="#"
-                       class="w-10 h-10 bg-base-200 hover:text-primary text-base-content/60  rounded-full flex items-center justify-center transition group shadow-lg overflow-hidden">
-                        <x-filament::icon icon="fab-instagram" class="w-5 h-5 "/>
-                    </a>
-                    <!-- WhatsApp -->
-                    <a href="#"
-                       class="w-10 h-10 bg-base-200 hover:text-primary text-base-content/60  rounded-full flex items-center justify-center transition group shadow-lg overflow-hidden">
-                        <x-filament::icon icon="fab-whatsapp" class="w-5 h-5 "/>
-                    </a>
+                <div class="flex flex-col gap-y-3">
+                    <h3 class="text-text-light text-xl font-semibold">Nosso Endereço</h3>
+                    <p class="text-text-medium font-medium">Dr. Cardoso de Mello, 1666, Cj, 92 Vila Olímpia, São Paulo
+                    </p>
                 </div>
             </div>
 
-            <!-- Column 2: Links de Serviços -->
             <div>
-                <h4 class="text-xl font-semibold text-base-content mb-4">Links de Serviços</h4>
-                <ul class="space-y-2 ">
+                <h4 class="text-xl font-semibold text-text-light mb-4">Links de Serviços</h4>
+                <ul class="space-y-2 text-text-medium">
                     <li>
                         <a href="/key-account" class="hover:text-secondary transition">
                             Key Account
@@ -64,37 +49,32 @@
                 </ul>
             </div>
 
-            <!-- Column 3: Informações de Contato -->
-            <div class="flex justify-evenly flex-col gap-2">
-                <div>
-                    <h4 class="text-xl font-semibold text-base-content mb-2">Nosso Endereço</h4>
-                    <p class="mb-4 text-base-content/90">
-                        Dr. Cardoso de Mello, 1666, Cj. 92<br/>
-                        Vila Olímpia, São Paulo.
-                    </p>
-                </div>
-                <div>
-                    <h4 class="text-xl font-semibold text-base-content mb-2">Nosso Endereço</h4>
-                    <p class="font-medium text-base-content/90">contato@firece.com.br</p>
-                    <p class="mt-2 text-base-content/90">+55 11 98720-1303</p>
+            <div class="flex justify-evenly flex-col gap-y-4">
+                <h4 class="text-xl font-semibold text-text-light">Contato e endereço</h4>
+                <p class="font-medium text-text-medium">contato@firece.com.br</p>
+                <div class="flex items-center gap-2">
+                    <img src="{{ asset('images/brasil-flag.png') }}" alt="Phone"
+                        class="w-7 h-5 object-contain rounded-sm">
+                    <p class="text-text-medium">(11) 98720-1303</p>
                 </div>
             </div>
-            <!-- Column 4: Newsletter -->
-            <div>
-                <h4 class="text-xl font-semibold text-base-content mb-4">Nossa Newsletter</h4>
-                <p class="text-base-content/90 mb-6">
+
+            <div class="flex flex-col gap-y-4 col-span-2">
+                <h4 class="text-xl font-semibold text-text-light">Nossa Newsletter</h4>
+                <p class="text-text-medium">
                     Envie nos o seu email e receba as melhores notícias e textos sobre o que
                     acontece no mercado financeiro
                 </p>
-                <form class="flex flex-col sm:flex-row items-center sm:space-x-3 space-y-3 sm:space-y-0">
+                <form class="flex flex-col sm:flex-row items-center justify-between gap-x-4">
                     <input type="email" placeholder="Seu email" id="email"
-                           class="px-4 py-3 border border-base-200/70 bg-base-200/50 rounded-lg focus:border-primary focus:outline-none">
-                    <button type="submit"
-                            class="bg-neutral text-primary-content px-4 py-3 rounded-lg hover:bg-neutral transition-colors hover:bg-primary hover:text-primary-content focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none">
-                        Ok
-                    </button>
+                        class="px-4 py-3 border border-outline-dark rounded-lg focus:border-primary focus:outline-none text-text-light bg-transparent placeholder:text-text-medium w-2/3">
+                    <x-layout.shared.button type="submit"
+                        class="bg-primary text-primary-content px-4 py-3 rounded-lg hover:bg-primary hover:text-primary-content focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none text-text-medium w-1/3">
+                        Inscrever-se
+                    </x-layout.shared.button>
                 </form>
             </div>
+
         </div>
     </div>
 
