bfdca298938b4057e9272be35970643f8b5bf1a0
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-07-17 15:51:43 -0300

    test(custom-component-command): should fail if the component is already registered at template renderer

--- tests/Feature/Commands/CmsUpdateTemplateRenderCommandTest.php
+++ tests/Feature/Commands/CmsUpdateTemplateRenderCommandTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Filament\Components\Landing\MainHeroComponent;
 use App\Filament\Resources\AuthorResource\Pages\ListAuthors;
 use App\View\TemplateRenderer;
 
@@ -28,4 +29,13 @@ it('should throws exception if file does not exists', function (): void {
         'field' => 'list-authors',
     ]);
 })->throws(Exception::class);
+
+it('should fail if the component is already registered', function (): void {
+    $this->artisan('cms:update-templaterender', [
+        'class' => MainHeroComponent::class,
+        'field' => 'main_hero',
+    ])
+        ->expectsOutput('Component main_hero already registered at TemplateRender.')
+        ->assertExitCode(1);
+});
 
