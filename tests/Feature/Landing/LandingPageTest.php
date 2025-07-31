9e9c59bb8d935c52ffb1b6fe2ac9170c44b55cda
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-07-31 17:16:20 -0300

    test(testimonial-image-rendering): making sure that pages without testimonial should not render the testimonial avatar

--- tests/Feature/Landing/LandingPageTest.php
+++ tests/Feature/Landing/LandingPageTest.php
@@ -4,18 +4,19 @@ use App\Models\CMS\Page;
 use App\Models\Testimonial;
 use Database\Seeders\CMS\MenusSeeder;
 use Database\Seeders\CMS\PagesSeeder;
-use Database\Seeders\CMS\PostsSeeder;
 use Database\Seeders\TestimonialsSeeder;
 use Illuminate\Support\Str;
 
 use function Pest\Laravel\get;
 use function Pest\Laravel\seed;
 
-it('should loads the avatar_image on pages that has testimonials', closure: function () {
+beforeEach(function () {
     seed(TestimonialsSeeder::class);
     seed(PagesSeeder::class);
     seed(MenusSeeder::class);
+});
 
+it('should loads the avatar_image on pages that has testimonials', closure: function () {
     $testimonial = Testimonial::query()->first();
 
     $avatar = Str::remove('http://localhost:8000/storage/1/', $testimonial->getFirstMediaUrl('avatar'));
@@ -25,5 +26,20 @@ it('should loads the avatar_image on pages that has testimonials', closure: func
         $response = get(route('page.show', $page->slug));
         $response->assertSuccessful();
         $response->assertSee($avatar);
+    });
+});
+
+test('pages without testimonial should not render the testimonial avatar', function () {
+    $testimonial = Testimonial::query()->first();
+
+    $avatar = Str::remove('http://localhost:8000/storage/1/', $testimonial->getFirstMediaUrl('avatar'));
+    $pages = Page::query()
+        ->whereNotIn('slug', ['code-capital', '/', 'key-account'])
+        ->where('status','published')->get();
+
+    $pages->each(function ($page) use ($avatar) {
+        $response = get(route('page.show', $page->slug));
+        $response->assertSuccessful();
+        $response->assertDontSee($avatar);
     });
 });
