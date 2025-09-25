ff6eb887be49c6656bda383aef2dfc2abf9ebfc6
Julia <juliamesquita267@gmail.com>
2025-09-24 23:04:57 -0300

    wip

--- resources/views/components/sections/split-with-image-quote.blade.php
+++ resources/views/components/sections/split-with-image-quote.blade.php
@@ -21,8 +21,7 @@
     ]),
 ])
 
-<section id="nosso-atendimento"
-         class="mx-auto text-text-dark dark:text-text-light overflow-hidden py-8 md:py-16 lg:py-20">
+<section class="mx-auto text-text-dark dark:text-text-light overflow-hidden py-8 md:py-16 lg:py-20">
     <div class="mx-auto container px-4 md:px-6 lg:flex lg:items-center lg:justify-between lg:space-x-12 relative z-10">
         <div class="lg:w-1/2 mb-8 md:mb-12 lg:mb-0 space-y-6 md:space-y-8 lg:space-y-10">
             <x-layout.shared.chip class="px-4 py-2">
@@ -41,14 +40,14 @@
         </div>
 
         <div class="lg:w-1/2 relative pb-6 md:pb-10 md:pt-10 md:pr-10 md:pl-10">
-            <div class="hidden md:block md:absolute bottom-0 left-0 rotate-270">
-                <x-partials.corner variant="brand" class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] md:w-[120px] md:h-[120px]"/>
-                {{--                <div class="w-20 md:w-32 h-[8px] md:h-[13px] bg-brand-primary"></div>--}}
-                {{--                <div class="absolute bottom-0 left-0 h-20 md:h-32 w-[8px] md:w-[13px] bg-brand-primary"></div>--}}
-            </div>
-            <div class="hidden md:block md:absolute top-0 right-0 rotate-90">
-                <x-partials.corner variant="brand" class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] md:w-[120px] md:h-[120px]"/>
-            </div>
+{{--            <div class="hidden md:block md:absolute bottom-0 left-0 rotate-270">--}}
+{{--                <x-partials.corner variant="brand" class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] md:w-[120px] md:h-[120px]"/>--}}
+{{--                --}}{{--                <div class="w-20 md:w-32 h-[8px] md:h-[13px] bg-brand-primary"></div>--}}
+{{--                --}}{{--                <div class="absolute bottom-0 left-0 h-20 md:h-32 w-[8px] md:w-[13px] bg-brand-primary"></div>--}}
+{{--            </div>--}}
+{{--            <div class="hidden md:block md:absolute top-0 right-0 rotate-90">--}}
+{{--                <x-partials.corner variant="brand" class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] md:w-[120px] md:h-[120px]"/>--}}
+{{--            </div>--}}
 
             <div class="grid grid-cols-auto-fit gap-4 md:gap-6 lg:gap-10" style="grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));">
                 @foreach ($cards as $index => $card)
