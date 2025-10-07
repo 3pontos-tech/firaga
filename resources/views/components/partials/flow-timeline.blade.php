93eb6f5961d6991025758d7b6d432c4cb4b15c48
kaster <diogokaster@gmail.com>
2025-10-07 14:47:06 -0300

    feat(flow-timeline): adjust responsiveness and add a corner partial

--- resources/views/components/partials/flow-timeline.blade.php
+++ resources/views/components/partials/flow-timeline.blade.php
@@ -5,7 +5,7 @@
 <section class="py-8 sm:py-12 md:py-16 text-white">
     <div class="container grid grid-cols-1 md:grid-cols-3 md:grid-rows-3 mx-auto space-y-16 relative">
         @foreach($timeline as $item)
-            <div class="items-center space-y-4 bg-elevation-surface md:col-start-1 md:row-start-1
+            <div class="relative items-center z-10 p-4 md:p-0 bg-elevation-surface space-y-4 md:col-start-1 md:row-start-1
             {{ $loop->iteration === 2 ? 'md:col-start-2 md:row-start-2' : '' }}
             {{ $loop->iteration === 3 ? 'md:col-start-3 md:row-start-3' : '' }}">
                 <div class="flex items-center gap-4">
@@ -18,14 +18,21 @@
                 @php
                     $afterClasses = $loop->last
                         ? 'after:content-none'
-                        : "after:content-[''] after:block after:absolute after:-z-10 after:border-dashed
-                        after:md:border-b-2 after:border-l-2 after:border-text-medium after:left-1/2 after:md:left-1/10 after:top-[100%]
-                        after:w-[520px] after:h-[200px]";
+                        : "after:content-[''] after:max-w-[190px] after:lg:max-w-screen after:z-0 after:rounded-bl-xl
+                        after:absolute after:-z-10 after:border-dashed
+                        after:md:border-b-2 after:border-l-2 after:border-outline-low after:left-1/2 after:md:left-1/10
+                        after:md:top-[101%] after:w-[520px] after:h-[190px] after:top-[110%]";
                 @endphp
 
-                <x-card-v2 class="relative z-10 flex flex-row max-w-[650px] p-8 space-x-4 md:items-center border {{ $afterClasses }}">
+                @if($loop->iteration === 2)
+                    <div class="hidden lg:block lg:absolute top-4 -right-10 rotate-90">
+                        <x-partials.corner variant="brand" class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] md:w-[120px] md:h-[80px]"/>
+                    </div>
+                @endif
+
+                <x-card-v2 class="relative z-10 flex sm:flex-row max-w-[650px] p-8 space-x-4 sm:items-center border {{ $afterClasses }}">
                     <x-slot:icon>
-                        <div class="bg-outline-dark p-4 rounded-md">
+                        <div class="bg-outline-dark p-4 rounded-md max-w-fit">
                             <x-dynamic-component
                                 :component="$item['card_icon']"
                                 class="w-6 h-6"
