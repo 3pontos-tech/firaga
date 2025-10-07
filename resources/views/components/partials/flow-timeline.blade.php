4158b890736090721d817f7e119766aae605dcf8
kaster <diogokaster@gmail.com>
2025-10-07 12:38:14 -0300

    feat(flow-timeline): change component dashed line

--- resources/views/components/partials/flow-timeline.blade.php
+++ resources/views/components/partials/flow-timeline.blade.php
@@ -2,17 +2,12 @@
     'timeline'
 ])
 
-<section class="py-8 sm:py-12 md:py-16">
-    <div class="container grid grid-cols-3 grid-rows-3 mx-auto space-y-8 relative text-text-high">
+<section class="py-8 sm:py-12 md:py-16 text-white">
+    <div class="container grid grid-cols-1 md:grid-cols-3 md:grid-rows-3 mx-auto space-y-16 relative">
         @foreach($timeline as $item)
-            <img src="{{ asset('images/dashed-line-1.svg') }}" alt="Process Background"
-                 class="absolute xl:top-[230px] xl:left-[80px] lg:top-[354px] lg:left-[50px]">
-            <img src="{{ asset('images/dashed-line-1.svg') }}" alt="Process Background"
-                 class="absolute 2xl:top-[615px] 2xl:left-[735px] xl:top-[649px] xl:left-[500px] lg:left-[445px] lg:top-[750px]">
-            <div class="items-center space-y-4
-                {{ $loop->iteration === 1 ? 'col-start-1 row-start-1' : '' }}
-                {{ $loop->iteration === 2 ? 'col-start-2 row-start-2' : '' }}
-                {{ $loop->iteration === 3 ? 'col-start-3 row-start-3' : '' }}">
+            <div class="items-center space-y-4 bg-elevation-surface md:col-start-1 md:row-start-1
+            {{ $loop->iteration === 2 ? 'md:col-start-2 md:row-start-2' : '' }}
+            {{ $loop->iteration === 3 ? 'md:col-start-3 md:row-start-3' : '' }}">
                 <div class="flex items-center gap-4">
                     <span class="bg-brand-primary text-2xl w-8 h-8 rounded-full flex items-center justify-center">
                         {{ $loop->iteration }}
@@ -20,7 +15,15 @@
                     <h3 class="text-xl">{{ $item['title'] }}</h3>
                 </div>
 
-                <x-card-v2 class="relative z-10 flex flex-row max-w-[650px] p-8 space-x-4 items-center">
+                @php
+                    $afterClasses = $loop->last
+                        ? 'after:content-none'
+                        : "after:content-[''] after:block after:absolute after:-z-10 after:border-dashed
+                        after:md:border-b-2 after:border-l-2 after:border-text-medium after:left-1/2 after:md:left-1/10 after:top-[100%]
+                        after:w-[520px] after:h-[200px]";
+                @endphp
+
+                <x-card-v2 class="relative z-10 flex flex-row max-w-[650px] p-8 space-x-4 md:items-center border {{ $afterClasses }}">
                     <x-slot:icon>
                         <div class="bg-outline-dark p-4 rounded-md">
                             <x-dynamic-component
