93d6fb709cbf0c7d9e7e586e8e96965e3d48e4df
kaster <diogokaster@gmail.com>
2025-10-08 15:25:03 -0300

    refactor(faq): improve mobile ui and image

--- resources/views/components/partials/faq.blade.php
+++ resources/views/components/partials/faq.blade.php
@@ -2,23 +2,25 @@
 @props([
     'solutions' => collect(),
 ])
-<div class="py-8 sm:py-12 md:py-16 lg:py-20 xl:py-60">
-    <div class="container mx-auto px-4 md:px-6">
-        <div class="flex flex-col lg:flex-row gap-8 lg:gap-12 justify-between items-start lg:items-center">
-            <div class="relative hidden md:block lg:block w-full lg:w-1/2">
+<div class="container mx-auto py-8 sm:py-12 md:py-16 lg:py-20 xl:py-60">
+    <div class="px-4 md:px-6">
+        <div class="flex flex-col bg-elevation-01dp border border-outline-dark rounded-xl transition lg:grid grid-cols-7 gap-8
+            lg:gap-12 items-stretch">
+            <div class="relative w-full h-full col-span-2">
                 <img alt="Customer support representative helping customers" loading="lazy" decoding="async"
-                     class="rounded-lg object-cover w-full h-auto" style="color: transparent;"
+                     class="rounded-lg object-cover w-full h-full" style="color: transparent;"
                      src="{{ $renderable?->getFirstMediaUrl(CustomComponent::Faq->value) ?? asset('images/stock/hero-stock.png') }}">
             </div>
-            <div class="w-full lg:w-1/2">
+            <div class="w-full p-8 lg:p-15 lg:col-span-5">
                 <div class="flex flex-col gap-y-4 sm:gap-y-6 md:gap-y-8">
-                    <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-text-high">
+                    <h2 class="text-xl sm:text-3xl md:text-4xl font-bold text-text-high">
                         Perguntas Frequentes
                     </h2>
                 </div>
                 <div class="w-full space-y-4 sm:space-y-6 md:space-y-8 mt-6 sm:mt-8">
                     @forelse($solutions as $index => $solution)
-                        <div class="transition-all duration-300 ease-in-out hover:bg-elevation-01dp rounded-lg p-3 sm:p-4"
+                        <div class="transition-all duration-300 ease-in-out hover:outline hover:outline-outline-dark
+                            hover:bg-elevation-02dp rounded-lg p-3 sm:p-4"
                              x-data="{ open: false }">
                             <h3 class="flex">
                                 <button type="button" @click="open = !open" :aria-expanded="open"
