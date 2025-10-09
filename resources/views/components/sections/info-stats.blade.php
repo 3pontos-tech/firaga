5ab39b44e86229a1df2dd6d72f555d431c37d75b
kaster <diogokaster@gmail.com>
2025-10-09 09:09:55 -0300

    feat(infinite-scroll): add a mask to have a smoother fading out of the cards

--- resources/views/components/sections/info-stats.blade.php
+++ resources/views/components/sections/info-stats.blade.php
@@ -13,14 +13,14 @@
         </div>
 
         <!-- Infinite scroll wrapper -->
-        <div class="col-span-8 overflow-hidden w-full my-auto">
+        <div class="mask-l-from-95% mask-l-to-99% mask-r-from-95% mask-r-to-99% p-4 col-span-8 overflow-hidden w-full my-auto">
             <div id="metrics" class="flex animate-infinite-scroll whitespace-nowrap gap-6">
 
                 <!-- First set of cards -->
                 @foreach(range(1,2) as $ignored)
                 <div class="flex gap-6">
                     @foreach ($metrics as $metric)
-                        <div class="shrink-0 w-[15rem] sm:w-[18rem]">
+                        <div class="shrink-0 w-[15rem] sm:w-[18rem] transition-transform duration-300 ease-in-out hover:scale-[1.02]">
                             <x-card-v2 :card="$metric" text-box="mt-8 flex-col" emphasis="primary" :interactive="true" class="h-full">
                                 <x-slot:title class="text-2xl!">{{ $metric->title }}</x-slot:title>
                                 <x-slot:description>{{ $metric->description }}</x-slot:description>
