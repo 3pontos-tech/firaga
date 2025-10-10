8f35dfe8cb9c90c013192a5e26b122d1b7878dc0
kaster <diogokaster@gmail.com>
2025-10-10 13:30:57 -0300

    refactor(cta-with-icon): change tablet display

--- resources/views/components/sections/cta-with-icon.blade.php
+++ resources/views/components/sections/cta-with-icon.blade.php
@@ -10,7 +10,7 @@
     class="text-base-content py-12 sm:py-16 md:py-20 lg:py-24 xl:py-30 overflow-hidden bg-elevation-surface container mx-auto">
     <div class="bg-elevation-02dp rounded-2xl sm:rounded-3xl p-4 sm:p-6 md:p-8 lg:p-10">
         <div
-            class="flex flex-col sm:flex-row items-center justify-between gap-8 sm:gap-8 md:gap-12 lg:gap-16 xl:gap-20
+            class="flex flex-col lg:flex-row items-center justify-between gap-8 sm:gap-8 md:gap-12 lg:gap-16 xl:gap-20
                 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 py-8 sm:py-12 md:py-16 lg:py-20 xl:py-24">
             <div class="relative">
                 <span
@@ -23,7 +23,7 @@
                          class="w-12 h-12 sm:w-16 sm:h-16 md:w-18 md:h-18 lg:w-20 lg:h-20 text-text-high"/>
                 </span>
             </div>
-            <div class="flex-1 text-center sm:text-left mb-6 lg:mb-0 px-2 sm:px-0 order-2 lg:order-2">
+            <div class="flex-1 text-center lg:text-left mb-6 lg:mb-0 px-2 sm:px-0 order-2 lg:order-2">
                 <h3 class="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-text-dark dark:text-text-light
                     font-extrabold mb-3 sm:mb-4 leading-tight">
                     {{ $title }}
