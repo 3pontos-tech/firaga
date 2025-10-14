6057c7fe4aaf6363c2c03a7f42207f852a87fa41
kaster <diogokaster@gmail.com>
2025-10-14 10:03:55 -0300

    feat(ui): adjust card alignment and styles in split-vertical-three-steps section

--- resources/views/components/sections/split-vertical-three-steps.blade.php
+++ resources/views/components/sections/split-vertical-three-steps.blade.php
@@ -23,12 +23,12 @@
                     <x-card-v2
                             :card="$card"
                             :interactive="true"
-                            text-box="flex flex-col gap-2 md:mx-4 sm:items-start sm:text-left"
+                            text-box="flex flex-col gap-2 md:mx-4 text-center sm:items-start sm:text-left"
                             class="flex flex-col gap-4 m-0! justify-center items-center
                             sm:justify-start text-center h-full sm:flex-row"
                     >
-                        <x-slot:icon class="bg-brand-primary rounded-lg group-hover/card:bg-brand-accent h-12 w-12 sm:h-16 sm:w-16 sm:min-w-16 sm:min-h-16 flex items-center justify-center">
-                            <x-filament::icon :icon="$card->icon" class="group-hover/card:text-brand-primary   w-6 h-6 sm:w-8 sm:h-8" />
+                        <x-slot:icon class="bg-outline-dark rounded-lg group-hover/card:bg-brand-accent h-12 w-12 sm:h-16 sm:w-16 sm:min-w-16 sm:min-h-16 flex items-center justify-center">
+                            <x-filament::icon :icon="$card->icon" class="group-hover/card:text-brand-primary w-6 h-6 sm:w-8 sm:h-8" />
                         </x-slot:icon>
 
                         <x-slot:title class="text-center sm:text-left">
