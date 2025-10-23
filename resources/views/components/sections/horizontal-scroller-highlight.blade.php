856905bca2acd3e628d74d400c2b188aa4109e1d
kaster <diogokaster@gmail.com>
2025-10-22 21:23:17 -0300

    hotfix: fix slot description whitespace handling and clean up unused spacing adjustments

--- resources/views/components/sections/horizontal-scroller-highlight.blade.php
+++ resources/views/components/sections/horizontal-scroller-highlight.blade.php
@@ -9,17 +9,20 @@
             <x-headline :component="$headline" />
         </div>
         <div class="overflow-hidden w-full my-auto">
-            <div class="mx-4 sm:mx-5 flex animate-infinite-scroll whitespace-nowrap gap-6 lg:animate-none rounded-xl sm:rounded-2xl">
+            <div class="mx-4 sm:mx-5 flex animate-infinite-scroll gap-6 lg:animate-none rounded-xl sm:rounded-2xl">
                 @foreach(range(1,2) as $idx)
                     <div class="flex {{ $idx == 2 ? 'lg:hidden' : 'lg:grid' }} mx-auto lg:grid-cols-5 lg:min-w-[300px] gap-4 sm:gap-6 md:gap-8 min-h-[240px] items-end">
                         @foreach($cards as $card)
                             <x-card-v2
-                                    :$card
-                                    class="min-w-[250px] animate-pulse hover:animate-none"
-                                    text-box="mt-2 gap-2 flex-col transition-discrete! duration-400 ease-in-out" emphasis="primary" :interactive="true"
+                                :$card
+                                class="min-w-[250px] animate-pulse hover:animate-none"
+                                text-box="mt-2 gap-2 flex-col transition-discrete! duration-400 ease-in-out" emphasis="primary" :interactive="true"
                             >
                                 <x-slot:title class="text-3xl! ">{{ $card->title }}</x-slot:title>
-                                <x-slot:description class="overflow-hidden break-words">{{ $card->description }}</x-slot:description>
+                                <x-slot:description class="whitespace-normal break-words min-h-[50px]">
+                                    {{ Str::limit($card->description) }}
+                                </x-slot:description>
+
                                 <x-slot:actions class="hidden transition ease-in-out group-hover/card:flex">
                                     @foreach($card->actions as $action)
                                         <div class="flex flex-row items-center gap-2">
@@ -30,7 +33,6 @@
                                 </x-slot:actions>
                             </x-card-v2>
                         @endforeach
-
                     </div>
                 @endforeach
             </div>
