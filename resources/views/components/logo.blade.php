bd2e9aa83915e3eef441b8740bd45a997b841dcc
kaster <diogokaster@gmail.com>
2026-04-22 17:08:24 -0300

    chore: add logo-with-text Blade component, update logo styles, and replace logo-white.svg with logo-with-text.svg

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,12 @@
+@props (['size' => 'md'])
+
+@php
+    $height = match ($size) {
+        'sm' => '14.5px',
+        default => '22px',
+    };
+@endphp
+
 <a href="{{ route('home') }}" {{ $attributes }}>
-    <img src="{{ asset('images/logos/logo.svg') }}" alt="{{ config('app.name') }}" class="h-6" />
+    <img src="{{ asset('images/logos/logo.svg') }}" alt="{{ config('app.name') }}" style="height: {{ $height }}" />
 </a>
